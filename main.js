3c8b6a481de2f9ac62c3c4cb72799c836636c7e3
luiz <110947302+luizchamomile@users.noreply.github.com>
2023-11-24 09:24:34 -0300

    a

--- main.js
+++ main.js
@@ -1,13 +1,21 @@
-function somTocaGrilo (){
-
-    document.querySelector("#som_tecla_grilo").play();
+function tocaSom (idElementoAudio) {
+    document.querySelector(idElementoAudio).play();
 }
 
-document.querySelector(".tecla_grilo").onclick = somTocaGrilo;
+const listaDeTeclas = document.querySelectorAll('.tecla');
 
-const listaDeTeclas = document.querySelectorAll(".tecla");
-listaDeTeclas[4].onclick = tocaSomGrilo;
+//para
+for (let contador = 0; contador < listaDeTeclas.length; contador++) {
 
+    const tecla = listaDeTeclas[contador];
+    const instrumento = tecla.classList[1];
+    const idAudio = `#som_${instrumento}`; //template string
+
+    tecla.onclick = function () {
+        tocaSom(idAudio);
+    }
+
+}
 
 
 
